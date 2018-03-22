ecfc1c85fb64adb347e34a75246e33bac227c9ae
ITeMPe <przepiorkowski.marcin@gmail.com>
2018-03-23 00:02:30 +0100

    wygenerowanie i wyswietlenie pozorowanej bazy danych

---
+++ src/Product.java
@@ -0,0 +1,18 @@
+import java.text.DecimalFormat;
+
+public class Product
+{
+	String name;
+	double price;
+	int code;
+
+	
+	
+	public Product(String name_, double price_, int code_)
+	{
+	name = name_;	
+	price = price_;
+	code = code_;
+	}
+
+}
--- src/SimplePointOfSale.java
+++ src/SimplePointOfSale.java
@@ -1,10 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class SimplePointOfSale
 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	 static List<Product> productsList = new ArrayList<Product>();
+	static Random gll = new Random();
+	public static void main(String[] args) 
+	{
+		initProductsList();
+		showProductsList();
 
 	}
 
+	
+	
+	
+	
+		static void initProductsList()
+		{
+			int size =100;
+			for(int i=0; i<size; i++)
+			{
+				productsList.add(new Product("Produkt_"+i,((int)(gll.nextDouble()*10000))/100.0 ,i));
+			}
+		}
+		
+		static void showProductsList()
+		{
+			for(int i=0; i<productsList.size();i++)
+			{
+				System.out.print(productsList.get(i).name+"  ");System.out.print(productsList.get(i).price+"   ");System.out.println(productsList.get(i).code);
+			}
+		}
+		
 }
+
